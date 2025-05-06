c7432d0de2a151b51c193fa7e45af5dbbbe3fd34
llarkss <polkovnikov332@mail.ru>
2025-05-07 00:23:31 +0300

    removed id from sql request

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $db = new PDO('mysql:host=127.0.0.1;dbname=user_db;charset=utf8mb4', 'root', '')
 $email = $_POST['email'];
 $pass = $_POST['password'];
 
-$sql = "SELECT id, CONCAT(firstname, ' ', lastname) as name, password FROM users WHERE email = '$email'";
+$sql = "SELECT CONCAT(firstname, ' ', lastname) as name, password FROM users WHERE email = '$email'";
 $result = $db->query($sql);
 $user = $result->fetch();
 
